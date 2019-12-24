34c30e854f8f61cd954022b0fde5f2a0635e11f0
amit223 <amit.moshe100@gmail.com>
2019-12-24 17:10:05 +0200

    anastasia

--- Anastasia.py
+++ Anastasia.py
@@ -1,11 +1,14 @@
 import simplejson
 import urllib
+import androguard.core.bytecodes.apk
 from androguard . core . bytecodes . dvm import *
 from androguard . core . bytecodes . apk import *
 from androguard . core . analysis . analysis import *
+from androguard.misc import AnalyzeAPK
+
 
 def getIntents(url):
-    a = APK ("Files/app1.apk")
+    a = APK (url)
     d = dvm . DalvikVMFormat ( a. get_dex () )
     z = d . get_strings ()
     intentList=[]
@@ -14,3 +17,13 @@ def getIntents(url):
             intents = z[i ]
             intentList . append ( intents )
     return intentList
+
+def getByteCode(url):
+    a,d,dx = AnalyzeAPK(url)
+    for method in dx.get_methods():
+        if method.is_external():
+            continue
+        m = method.get_method()
+        if m.get_code():
+            print(m.get_code().get_bc().get_raw())
+getByteCode("Files/app1.apk")
