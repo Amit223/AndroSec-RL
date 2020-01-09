94bae1540350643ae40ae6da6e41cd9b002f54ab
Inbaroth <inbaroth1993@gmail.com>
2020-01-09 16:26:15 +0200

    delete files from anastasia

--- Anastasia/AnastasiaDetector.py
+++ Anastasia/AnastasiaDetector.py
@@ -5,12 +5,12 @@ from Models.FeatureExtraction import get_intents_cmdcalls_apicalls
 
 class AnastasiaDetector:
     def __init__(self):
-        self.vectorizer=load("Anastasia/AnastasiaFeatures.joblib")
+        self.vectorizer = load("Anastasia/AnastasiaFeatures.joblib")
         self.feature_selector = load('Anastasia/AnastasiaFeaturesSelected.joblib')
         self.clf = load('Anastasia/AnastasiaClassifier.joblib')
 
     # this function return 1 if file is malware and 0 otherwise
-    def detect(self,file):
+    def detect(self, file):
         # feature extraction
         features = get_intents_cmdcalls_apicalls(file)
         # vectorize:
